119f6510229a81108c4e1c12e8c8e4c5bd2de1e0
Afolabi Esther <starafolab@gmail.com>
2023-08-18 03:55:10 +0100

    Adjust Layout (MUI)

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import './App.css';
-import { Routes, Route, NavLink, Link } from 'react-router-dom'
+import React from "react";
+import styles from "./App.module.css";
+// import Box from "@mui/material/Box";
+import Stack from '@mui/material/Stack';
+// import Container from '@mui/material/Container';
+import { Routes, Route, NavLink, Link } from "react-router-dom";
 import Home from './components/Home'
-import About from './components/About'
-import Contact from './components/Contact'
-import Project from './components/Project'
-
+// import About from './components/About'
+// import Contact from './components/Contact'
+// import Project from './components/Project'
 
 // Project inner
 // import Todo from './components/Todo'
@@ -14,71 +17,81 @@ import Project from './components/Project'
 // import Dashboard from './components/Dashboard'
 // import Review from './components/Review'
 
-
 function App() {
   return (
-    <div className="App">
+    <div className={styles["App"]}>
+      {/* ----------------------------- */}
+      {/* NAVIGATION */}
+      {/* ----------------------------- */}
+      <nav>
+        <div className={styles["nav-title"]}>Afolabi Esther</div>
+        <Stack direction="row" justifyContent="center" alignItems="center" spacing={4}>
+        <span>
+            <a href="https://github.com/Hestia-Arc" target="blank">
+            <i className="fa-brands fa-github icon"></i>
+            </a>
+        </span>
+        <span>
+            <a href="https://www.linkedin.com/in/hestiaarc00" target="blank">
+            <i className="fa-brands fa-linkedin icon"></i>
+            </a>
+        </span>
+        <span>
+            <a href="https://twitter.com/arc_hestia00" target="blank">
+            <i className="fa-brands fa-twitter icon"></i>
+            </a>
+        </span>
+        <span>
+            <a href="mailto:hestia.dev005@gmail.com">
+            <i className="fa-solid fa-envelope icon"></i>
+            </a>
+        </span>
+        <div className={styles["nav-btn"]}>
+        <Link to="contact">Contact</Link>
+        </div>
+  
+        </Stack>
+      </nav>
+    
 
-          {/* Navigation */}
-          <nav>
-              <div className='nav-title'>AFOLABI ESTHER</div>
 
-              {/* menu */}
-              <div className='nav-menu'>
-                  {/* Social Icons */}
-                  <div className="top-icon">
-                      <span><a href='https://github.com/Hestia-Arc' target='blank'><i className="fa-brands fa-github icon"></i></a></span>
-                      <span><a href='https://www.linkedin.com/in/hestiaarc00' target='blank'><i className="fa-brands fa-linkedin icon"></i></a></span>
-                      <span><a href='https://twitter.com/arc_hestia00' target='blank'><i className="fa-brands fa-twitter icon"></i></a></span>
-                      <span><a href='mailto:hestia.dev005@gmail.com'><i className="fa-solid fa-envelope icon"></i></a></span>
-                  </div>
-                  <div className='nav-btn'><Link to='contact'>Contact</Link></div>
-              </div>
-          </nav>
+      {/* contents */}
+      {/* Routes */}
+      <Routes>
+        <Route path='/' element={<Home />}/>
+        {/* <Route path='about' element={<About />} /> */}
+        {/* <Route path='who' element={<Who />}/> */}
+        {/* <Route path='skill' element={<Skill />}/> */}
+        {/* <Route path='service' element={<Service />}/> */}
 
-        
-              {/* contents */}
-             {/* Routes */}
-              <Routes>
-                  <Route path='/' element={<Home />}/>
-                  <Route path='about' element={<About />} />
-                      {/* <Route path='who' element={<Who />}/> */}
-                      {/* <Route path='skill' element={<Skill />}/> */}
-                      {/* <Route path='service' element={<Service />}/> */}
-                  
-                  <Route path='contact' element={<Contact />}/>
-                  <Route path='project' element={<Project />} />
-                     {/* <Route path='todo' element={<Todo />}/>
+        {/* <Route path='contact' element={<Contact />}/> */}
+        {/* <Route path='project' element={<Project />} /> */}
+        {/* <Route path='todo' element={<Todo />}/>
                      <Route path='blog' element={<Blog />}/>
                      <Route path='booking' element={<Booking />}/>
                      <Route path='chat' element={<Chat />}/>
                      <Route path='dashboard' element={<Dashboard />}/>
                      <Route path='review' element={<Review />}/> */}
-                
-              </Routes>
+      </Routes>
 
+      {/* ---------------------------- */}
+      {/* TABS */}
+      {/* ---------------------------- */}
+      <div className={styles["tabs-container"]}>
+        <span>
+            <footer>
+                <div className={styles["copyright"]}>Built by Esther &copy;2023 </div>
+                <div><a href="https://github.com/Hestia-Arc/Portfolio-vercel">Source code</a></div>
+            </footer>
+        </span>
 
-          {/* tabs */}
-          <div className='header-tabs-container'>
+        <hr className={styles['horizontal-line']}/>     
               
-              {/* Footer */}
-              <span>
-                <footer>
-                    <div className="copyright">Built by Esther &copy;2023 </div>
-                    {/* <div><a href="https://github.com/Hestia-Arc/Portfolio-vercel">Source code</a></div> */}
-                </footer>
-              </span>
-
-              <hr className='horizontal-line'/>
-              
-             
-              {/*  */}
-              <div className='tab'><NavLink to='/'>HOME</NavLink></div>
-              <div className='tab'><NavLink to='about'>ABOUT</NavLink></div>
-              <div className='tab'><NavLink to='project'>PROJECTS</NavLink></div>
-              <div className='tab'><NavLink to='contact'>CONTACT</NavLink></div>
-          </div>
-
+        <div className={styles['tab']}><NavLink to='/'>HOME</NavLink></div>
+        <div className={styles['tab']}><NavLink to='about'>ABOUT</NavLink></div>
+        <div className={styles['tab']}><NavLink to='project'>PROJECTS</NavLink></div>
+        <div className={styles['tab']}><NavLink to='contact'>CONTACT</NavLink></div>
+      </div>
     </div>
   );
 }
